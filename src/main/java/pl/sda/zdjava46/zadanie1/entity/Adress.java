1d0fbaf783516083e25bbcf05a1512b266b145c7
Karol011 <karollacek1@gmail.com>
2021-05-23 17:11:07 +0200

    setup db connection

--- src/main/java/pl/sda/zdjava46/zadanie1/entity/Adress.java
+++ src/main/java/pl/sda/zdjava46/zadanie1/entity/Adress.java
@@ -2,18 +2,18 @@ package pl.sda.zdjava46.zadanie1.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.Set;
 
 @Entity
 @Data
+@Table(name = "adress")
 public class Adress {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @OneToMany
+    @OneToMany(mappedBy = "adress")
     Set<User> users;
     private String street;
     private String zipCode;
--- src/main/java/pl/sda/zdjava46/zadanie1/entity/User.java
+++ src/main/java/pl/sda/zdjava46/zadanie1/entity/User.java
@@ -4,15 +4,15 @@ package pl.sda.zdjava46.zadanie1.entity;
 import lombok.Data;
 import pl.sda.zdjava46.zadanie1.repository.UserRepository;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity
 @Data
+@Table(name = "user")
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String surname;
